9c57baa6bbf2679d4a592cc745bf1856e5e54b6a
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-06-23 17:34:15 -0500

    Got Everything Setup to run Old React Course

--- assets/js/components/FirstComp.js
+++ assets/js/components/FirstComp.js
@@ -5,52 +5,40 @@ class Layout extends Component {
 	constructor() {
 		super();
 		this.state = {
-			name: 'Joe'
+			name: 'Nate',
+			health: 55,
+			level: 1,
+			test: 'this is a test'
 		};
 	}
 	clickedBtn = () => {};
+
 	async test() {}
+
 	render() {
 		return (
-			<div className="home">
-				<div className="Aligner">
-					<div className="Aligner-item">
-						<img src="/img/logo.png" />
-						<h1>Dev-Starter-Kit</h1>
-						<div className="menu">
-							<ul>
-								<li>
-									<a href="http://starterkit.codingphase.com" target="new">
-										Documentation
-									</a>
-								</li>
-								<li>
-									<a href="http://www.codingphase.com" target="new">
-										CodingPhase.Com
-									</a>
-								</li>
-							</ul>
-						</div>
-						<div className="version-num">version 4.0.0</div>
-						<br />
-						<a
-							className="github-button"
-							href="https://github.com/codingphasedotcom/Starter-Kit-2019"
-							data-icon="octicon-star"
-							data-style="mega"
-							data-count-href="/codingphasedotcom/rocky/stargazers"
-							data-count-api="/repos/codingphasedotcom/rocky#stargazers_count"
-							data-count-aria-label="# stargazers on GitHub"
-							aria-label="Star codingphasedotcom/rocky on GitHub"
-						>
-							Star
-						</a>
-					</div>
+			<div id={'parent'}>
+				<div className={`blue-bg`}>
+					<h3>Name: {this.state.name}</h3>
+					<h3>health: {this.state.health}</h3>
+					<h3>level: {this.state.level}</h3>
 				</div>
 			</div>
 		);
 	}
 }
+
+var Header = function() {
+	return (
+		<header>
+			<ul>
+				<li>Home</li>
+				<li>About</li>
+				<li>Contact</li>
+			</ul>
+		</header>
+	);
+};
 
 const app = document.getElementById('app');
 
